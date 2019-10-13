4aaeedbf7b9c81a39540b6ad7f9062a545a2a4f5
Sebastian Robaczewski <sr.sebastianrobaczewski@gmail.com>
2019-10-13 09:39:40 +0200

    dbcontext

---
+++ venditiun/Data/VenditiunDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using venditiun.Models;
+
+namespace venditum.Data
+{
+    public class VenditiunDbContext : IdentityDbContext
+    {
+        public VenditiunDbContext(DbContextOptions<VenditiunDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Project> Project { get; set; }
+    }
+}
